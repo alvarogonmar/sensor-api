ff1b71ad8f70be2842f89504e150a9f95e831697
Alvaro <alvarogonmar0@gmail.com>
2025-11-06 07:37:30 -0600

    Implement database connection using environment variable
    Added database connection functionality.

--- api/index.py
+++ api/index.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+CONNECTION_STRING = os.getenv("CONN_STRING")
+
+def get_connection():
+    return psycop2.connect(CONNECTION_STRING)
+
 @app.route('/')
 def home():
     return 'Hello, World!'
